2d6d07f491363477050586632b0b2b59a1b609cf
GeorgiEnev <150971611+MartinKasarov22@users.noreply.github.com>
2026-03-06 16:42:28 +0200

    load quiz questions when quiz starts

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,18 @@
+import { loadQuestions } from "./data-loader.js";
+
 const startButton = document.getElementById("start-btn");
 const startScreen = document.getElementById("start-screen");
 const quizScreen = document.getElementById("quiz-screen");
 
+let questions = [];
+
 startButton.addEventListener("click", startQuiz);
 
-function startQuiz() {
+async function startQuiz() {
   startScreen.classList.add("hidden");
   quizScreen.classList.remove("hidden");
+
+  questions = await loadQuestions();
+
+  console.log("Loaded questions:", questions);
 }
